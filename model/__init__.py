cb53774f9c272e69d187cb2e5ca5ec4b5f2cd0f3
Leonardo <leonardobora.lbz@gmail.com>
2025-09-23 17:41:02 -0300

    Atualização do projeto: ajustes no CSS, templates e lógica de distribuição

---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+
+from .atributos import Personagem, Atributos
+from .classe import ClassePersonagem, Guerreiro, Ladrao, Mago
+from .raca import Raca, Humano, Elfo, Anao, Halfling
+from .distribuicao import EstiloClassico, EstiloAventureiro, EstiloHeroico
+
+__all__ = [
+    "Personagem", "Atributos",
+    "ClassePersonagem", "Guerreiro", "Ladrao", "Mago",
+    "Raca", "Humano", "Elfo", "Anao", "Halfling",
+    "EstiloClassico", "EstiloAventureiro", "EstiloHeroico"
+]
---
+++ model/classe.py
@@ -0,0 +1,52 @@
+class ClassePersonagem:
+    def __init__(self, nome, vida, armas, armaduras, itens_magicos, habilidades):
+        self.nome = nome
+        self.vida = vida
+        self.armas = armas
+        self.armaduras = armaduras
+        self.itens_magicos = itens_magicos
+        self.habilidades = habilidades[:]
+
+    def exibir_info(self):
+        print(f"\n-- Classe: {self.nome} --")
+        print(f"Vida: {self.vida}")
+        print(f"Armas: {self.armas}")
+        print(f"Armaduras: {self.armaduras}")
+        print(f"Itens mágicos: {self.itens_magicos}")
+        print(f"Habilidades: {self.habilidades}")
+
+
+class Guerreiro(ClassePersonagem):
+    def __init__(self):
+        super().__init__(
+            nome="Guerreiro",
+            vida=10,
+            armas="Pode usar todas as armas.",
+            armaduras="Pode usar todas as armaduras.",
+            itens_magicos="Não pode usar cajados, varinhas e pergaminhos, exceto pergaminho de proteção.",
+            habilidades=["Ataque Extra", "Especialista em Combate"]
+        )
+
+
+class Ladrao(ClassePersonagem):
+    def __init__(self):
+        super().__init__(
+            nome="Ladrão",
+            vida=6,
+            armas="Apenas armas pequenas ou médias.",
+            armaduras="Apenas armaduras leves.",
+            itens_magicos="Não pode usar cajados, varinhas e pergaminhos, exceto pergaminhos de proteção.",
+            habilidades=["Ataque Furtivo", "Ouvir Ruídos", "Arrombar"]
+        )
+
+
+class Mago(ClassePersonagem):
+    def __init__(self):
+        super().__init__(
+            nome="Mago",
+            vida=4,
+            armas="Apenas armas pequenas.",
+            armaduras="Nenhuma; armaduras impedem conjuração.",
+            itens_magicos="Pode usar todos os tipos de itens mágicos.",
+            habilidades=["Ler Magias", "Detectar Magias", "Conjurar Magias"]
+        )
